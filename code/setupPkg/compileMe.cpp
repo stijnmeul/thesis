0b3d4a41743c6a3719d96410f429e9ea2bafdeaa
Stijn Meul <stijn.meul@gmail.com>
2014-04-07 15:29:54 +0200

    Added a basic environment for safely setting up a PKG

---
+++ code/setupPkg/compileMe.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, char* argv[])
+{
+    cout << endl;
+    cout << argv[1] << endl;
+    return 0;
+}
---
+++ code/setupPkg/ibe_pkg_setup.cpp
@@ -0,0 +1,69 @@
+/*
+   Boneh and Franklin IBE
+
+   Compile with modules as specified below
+
+   For MR_PAIRING_CP curve
+   cl /O2 /GX ibe.cpp cp_pair.cpp zzn2.cpp big.cpp zzn.cpp ecn.cpp miracl.lib
+
+   For MR_PAIRING_MNT curve
+   cl /O2 /GX ibe.cpp mnt_pair.cpp zzn6a.cpp ecn3.cpp zzn3.cpp zzn2.cpp big.cpp zzn.cpp ecn.cpp miracl.lib
+
+   For MR_PAIRING_BN curve
+   cl /O2 /GX ibe.cpp bn_pair.cpp zzn12a.cpp ecn2.cpp zzn4.cpp zzn2.cpp big.cpp zzn.cpp ecn.cpp miracl.lib
+
+   For MR_PAIRING_KSS curve
+   cl /O2 /GX ibe.cpp kss_pair.cpp zzn18.cpp zzn6.cpp ecn3.cpp zzn3.cpp big.cpp zzn.cpp ecn.cpp miracl.lib
+
+   For MR_PAIRING_BLS curve
+   cl /O2 /GX ibe.cpp bls_pair.cpp zzn24.cpp zzn8.cpp zzn4.cpp zzn2.cpp ecn4.cpp big.cpp zzn.cpp ecn.cpp miracl.lib
+
+   See https://eprint.iacr.org/2001/090 for more information
+   Section 4.1 and 4.2
+*/
+
+//********* choose just one of these pairs **********
+//#define MR_PAIRING_CP      // AES-80 security
+//#define AES_SECURITY 80
+
+//#define MR_PAIRING_MNT	// AES-80 security
+//#define AES_SECURITY 80
+
+//#define MR_PAIRING_BN    // AES-128 or AES-192 security
+//#define AES_SECURITY 128
+//#define AES_SECURITY 192
+
+//#define MR_PAIRING_KSS    // AES-192 security
+//#define AES_SECURITY 192
+
+#define MR_PAIRING_BLS    // AES-256 security
+#define AES_SECURITY 256
+//*********************************************
+
+#include "../cppmiracl/source/pairing_3.h"
+#include "ibe_pkg.h"
+#include <termios.h>
+#include <unistd.h>
+
+int main()
+{
+	// Ask for a password to encrypt the new setup key.
+    termios oldt;
+
+    cout << "Please insert a password to encrypt the master secret key:";
+    // Turn off terminal output
+    tcgetattr(STDIN_FILENO, &oldt);
+    termios newt = oldt;
+    newt.c_lflag &= ~ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+    string s;
+    getline(cin, s);
+
+    // Turn terminal output back on.
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    string command = "./compileMe ";
+    command = command + s;
+    system(command.c_str());
+    return 0;
+}
