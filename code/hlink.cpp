73767b1a95953bc9de845005c6839cac1ed4e9f9
Stijn Meul <stijn.meul@gmail.com>
2014-05-04 09:44:20 +0200

    Added a hardlinking program and a hardlink to index.php

---
+++ code/hlink.cpp
@@ -0,0 +1,35 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+/*
+On Mac OSX, we can't create hard links on directories using the ln command..
+
+Install:
+ gcc hlink.cpp -o hardlink
+ To create a hard link:
+    hardlink source destination
+ To remove the link:
+    hardlink -u destination
+*/
+int main(int argc, char* argv[]) {
+    //Make sure we have the right arguments
+    if (argc != 3)
+    {
+        fprintf(stderr,"Usage:\thardlink source destination\n");
+        fprintf(stderr,"\t  hard links the source directory to the destination\n");
+        fprintf(stderr,"\thardlink -u destination\n");
+        fprintf(stderr,"\t  unlinks the destination directory\n");
+        return 1;
+    }
+    int ret = 0;
+    if(strcmp(argv[1], "-u") == 0)
+    {
+        ret = unlink(argv[2]);
+    }
+    else
+        ret = link(argv[1],argv[2]);
+
+    if (ret != 0)
+        perror("hardlink");
+    return ret;
+}
